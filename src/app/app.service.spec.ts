ff89f221795447c7354a809f09e0383c197f6957
Yogesh <borole116@gmail.com>
2022-04-23 22:45:46 +0530

    Initial commit

---
+++ src/app/app.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AppService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
